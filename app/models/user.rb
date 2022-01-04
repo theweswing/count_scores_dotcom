c1d6666a482584e71b9f9136540ed9aa2ede1a75
Wes Schierenbeck <wschierenbeck@gmail.com>
2022-01-04 11:30:40 -0500

    seeding

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :players
   has_many :matches, through: :players
+  has_many :games, through: :matches
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
